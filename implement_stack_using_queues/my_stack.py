99a840ea5c6ee9be22432641c0d222853cae356b
Oleksii-Lasiichuk <oleks1297@gmail.com>
2025-04-07 01:08:21 +0300

    my stack implementation using two queues

--- implement_stack_using_queues/my_stack.py
+++ implement_stack_using_queues/my_stack.py
@@ -1,4 +1,3 @@
-
 class Node:
     def __init__(self, item, next=None):
         self.item = item
@@ -22,14 +21,18 @@ class Queue:
 
     def pop(self):
         if self.head:
-            item = self.head
+            item = self.head.item  # Return the item, not the Node
             self.head = self.head.next
+            if self.head is None:
+                self.tail = None
             return item
         raise ValueError('Queue is empty.')
 
     @property
     def peek(self):
-        return self.head.item
+        if self.head:
+            return self.head.item  # Return the item, not the Node
+        raise ValueError('Queue is empty.')
 
     def __len__(self):
         count = 0
@@ -39,29 +42,39 @@ class Queue:
             current = current.next
         return count
 
+    def __bool__(self):
+        return not self.is_empty()
+
     def __str__(self):
         s = ''
         current = self.head
         while current is not None:
-            s += str(current.item)+' '
+            s += str(current.item) + ' '
             current = current.next
         return f'start -> {s}<- end'
 
-
 class MyStack:
-
     def __init__(self):
-        self.queue1 = []
-        self.queue2 = []
+        self.queue1 = Queue()
+        self.queue2 = Queue()
 
     def push(self, x: int) -> None:
-        
+        self.queue1.add(x)
 
     def pop(self) -> int:
-        
+        while len(self.queue1) > 1:
+            self.queue2.add(self.queue1.pop())
+        item = self.queue1.pop()
+        self.queue1, self.queue2 = self.queue2, self.queue1
+        return item
 
     def top(self) -> int:
-        
+        while len(self.queue1) > 1:
+            self.queue2.add(self.queue1.pop())
+        item = self.queue1.peek
+        self.queue2.add(self.queue1.pop())
+        self.queue1, self.queue2 = self.queue2, self.queue1
+        return item
 
     def empty(self) -> bool:
-        
+        return not self.queue1 and not self.queue2
